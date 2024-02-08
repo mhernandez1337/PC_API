c2042d0076e2ea897ce1a366811caa9f776080f0
Miguel Hernandez <mhernandez@nevada.unr.edu>
2024-02-08 09:30:17 -0800

    Updated the name of the Argument class

--- app/Models/argument.php
+++ app/Models/argument.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class argument extends Model
+class Argument extends Model
 {
     use HasFactory;
 
