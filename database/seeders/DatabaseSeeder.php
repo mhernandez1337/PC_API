0e64cfac17e7546faaf26099a8c73fe6fa45e9d9
Miguel Hernandez <mhernandez@nevada.unr.edu>
2024-12-16 20:52:33 -0800

    Commented out the File and Argument seeders from DatabaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            FileSeeder::class,
-            ArgumentSeeder::class,
+            // FileSeeder::class,
+            // ArgumentSeeder::class,
             UserSeeder::class
         ]);
     }
